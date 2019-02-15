36befccd47c46e05b705e1541da2e4d59a1c21ad
Jesper Hodge <jesper@instaffo.de>
2019-02-15 23:09:18 +0100

    get article parser going

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question
-import feedparser
 
 app = Flask(__name__)
 ask = Ask(app, '/')
@@ -14,13 +13,6 @@ def launched():
 def events():
     speech = render_template("events")
     return statement(speech)
-
-# @ask.intent("ArticleIntent")
-# def article():
-#     NewsFeed = feedparser.parse("https://xmedias2.morgenweb.de/feed/202-alexa-advanced-de-kall.xml")
-#     entry = NewsFeed.entries[1]
-#     speech = entry["description"]
-#     return statement(speech)
 
 @ask.intent("DummyIntent", convert={})
 def dummy_intent(name):
--- article_parser.py
+++ article_parser.py
@@ -1,5 +1,7 @@
 import feedparser
 from html.parser import HTMLParser
+import pdb
+import time
 
 class StolenFromStackoverflow(HTMLParser):
     def __init__(self):
@@ -12,47 +14,62 @@ class StolenFromStackoverflow(HTMLParser):
     def get_data(self):
         return ''.join(self.fed)
 
-def strip_tags(html):
-    s = StolenFromStackoverflow()
-    s.feed(html)
-    return s.get_data()
+class FeedReader:
+    overall_feeds = {
+            'Das Wichtigste': 'http://www.morgenweb.de/feed/146-rss-das-wichtigste.xml',
+            'Sport': 'http://www.morgenweb.de/feed/50-rss-sport.xml',
+            'Politik': 'http://www.morgenweb.de/feed/55-politik-rss-feed.xml',
+            'Wirtschaft': 'http://www.morgenweb.de/feed/56-wirtschaft-rss-feed.xml',
+            'Kultur': 'http://www.morgenweb.de/feed/57-kultur-rss-feed.xml',
+            'Aus aller Welt': 'http://www.morgenweb.de/feed/58-vermischtes-rss-feed.xml',
+            'Wissenschaft': 'http://www.morgenweb.de/feed/68-rss-wissenschaft.xml',
+            'Kommentare': 'http://www.morgenweb.de/feed/69-rss-kommentare.xml',
+            'Freizeit': 'http://www.morgenweb.de/feed/70-rss-freizeit.xml',
+            'Reise': 'http://www.morgenweb.de/feed/71-rss-reise.xml',
+            'Auto': 'http://www.morgenweb.de/feed/72-rss-auto.xml'
+    }
 
-overall_feeds = {
-        'Das Wichtigste': 'http://www.morgenweb.de/feed/146-rss-das-wichtigste.xml',
-        'Sport': 'http://www.morgenweb.de/feed/50-rss-sport.xml',
-        'Politik': 'http://www.morgenweb.de/feed/55-politik-rss-feed.xml',
-        'Wirtschaft': 'http://www.morgenweb.de/feed/56-wirtschaft-rss-feed.xml',
-        'Kultur': 'http://www.morgenweb.de/feed/57-kultur-rss-feed.xml',
-        'Aus aller Welt': 'http://www.morgenweb.de/feed/58-vermischtes-rss-feed.xml',
-        'Wissenschaft': 'http://www.morgenweb.de/feed/68-rss-wissenschaft.xml',
-        'Kommentare': 'http://www.morgenweb.de/feed/69-rss-kommentare.xml',
-        'Freizeit': 'http://www.morgenweb.de/feed/70-rss-freizeit.xml',
-        'Reise': 'http://www.morgenweb.de/feed/71-rss-reise.xml',
-        'Auto': 'http://www.morgenweb.de/feed/72-rss-auto.xml'
-}
+    regional_feeds = {
+            'Unternehmen der Region': 'http://www.morgenweb.de/feed/59-rss-feed-unternehmen-der-region.xml',
+            'Die Adler Mannheim': 'http://www.morgenweb.de/feed/51-rss-adler-mannheim.xml',
+            'Rhein-Neckar Löwen': 'http://www.morgenweb.de/feed/52-rss-loewen.xml',
+            'SV Waldhof 07 ': 'http://www.morgenweb.de/feed/53-rss-sv-waldhof.xml',
+            'TSG 1899 Hoffenheim': 'http://www.morgenweb.de/feed/54-rss-hoffenheim.xml'
+    }
 
-regional_feeds = {
-        'Unternehmen der Region': 'http://www.morgenweb.de/feed/59-rss-feed-unternehmen-der-region.xml',
-        'Die Adler Mannheim': 'http://www.morgenweb.de/feed/51-rss-adler-mannheim.xml',
-        'Rhein-Neckar Löwen': 'http://www.morgenweb.de/feed/52-rss-loewen.xml',
-        'SV Waldhof 07 ': 'http://www.morgenweb.de/feed/53-rss-sv-waldhof.xml',
-        'TSG 1899 Hoffenheim': 'http://www.morgenweb.de/feed/54-rss-hoffenheim.xml'
-}
+    def __init__(self, category, numberArticles):
+        self.category = category
+        self.numberArticles = numberArticles
+        self.feed_url = self.overall_feeds[category] or self.regional_feeds[category]
 
-def getFeed(url):
-    NewsFeed = feedparser.parse(url)
-    return NewsFeed.entries[1]
+    def strip_tags(self, html):
+        s = StolenFromStackoverflow()
+        try:
+            s.feed(html)
+            return s.get_data()
+        except TypeError:
+            return html
 
-def printEntry(entry):
-    print(entry.keys())
-    print(strip_tags(entry["summary"]))
+    def getFeed(self):
+        # pdb.set_trace()
+        return feedparser.parse(self.feed_url)
 
-def runTest(url):
-    feed = getFeed(url)
-    printEntry(feed)
+    def getEntries(self):
+        return self.getFeed().entries[:self.numberArticles]
 
-def printFirstArticleContent(url):
-    feed = getFeed(url)
-    return entry["description"]
+    def printEntry(self, entry):
+        tags = ""
+        for tag in entry["tags"]:
+            tags += tag["term"]
+        print(tags)
+        print(entry.keys())
+        print(entry.title)
+        print(self.strip_tags(entry.summary))
+        print(entry.published)
 
-runTest(overall_feeds['Das Wichtigste'])
+    def runTest(self):
+        feed = self.getFeed()
+        for entry in self.getEntries():
+            self.printEntry(entry)
+
+reader = FeedReader('Das Wichtigste', 3).runTest()
