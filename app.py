0028382d191a5cd4f6bae55f7a6b9fe23a222585
Jesper Hodge <jesper@instaffo.de>
2019-02-15 16:07:03 +0100

    add a test article

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_ask import Ask, statement, question
+import feedparser
 
 app = Flask(__name__)
 ask = Ask(app, '/')
@@ -14,7 +15,12 @@ def events():
     speech = render_template("events")
     return statement(speech)
 
-
+@ask.intent("ArticleIntent")
+def article():
+    NewsFeed = feedparser.parse("https://xmedias2.morgenweb.de/feed/202-alexa-advanced-de-kall.xml")
+    entry = NewsFeed.entries[1]
+    speech = entry["description"]
+    return statement(speech)
 
 @ask.intent("DummyIntent", convert={})
 def dummy_intent(name):
--- article_parser.py
+++ article_parser.py
@@ -3,7 +3,8 @@ import feedparser
 NewsFeed = feedparser.parse("https://xmedias2.morgenweb.de/feed/202-alexa-advanced-de-kall.xml")
 entry = NewsFeed.entries[1]
 
-print(entry.keys())
-print(entry["dachzeile"])
-print(len(NewsFeed.entries))
-print(entry["description"])
+# print(entry.keys())
+# print(entry["dachzeile"])
+# print(len(NewsFeed.entries))
+# print(entry["description"])
+
