4a264dc0004cd8b3844b1ee0afd106660865b409
kafaoglu <github@kafaoglu.de>
2019-02-15 11:45:32 +0100

    Initital commit

---
+++ api.py
@@ -0,0 +1,5 @@
+import requests
+
+def events_api():
+    host = "https://www2.morgenweb.de/austausch/vk_abfrage.php?out=json"
+    params = "test"
---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_ask import Ask, statement, question
+
+app = Flask(__name__)
+ask = Ask(app, '/')
+
+@ask.launch
+def launched():
+    speech = render_template("welcome")
+    return question(speech)
+
+@ask.intent("EventsIntent")
+def events():
+    speech = render_template("events")
+    return statement(speech)
+
+
+
+@ask.intent("DummyIntent", convert={})
+def dummy_intent(name):
+    speech = "Freut mich {}".format(name)
+    return statement(speech)
+
+if __name__ == '__main__':
+    app.run(debug=True)
