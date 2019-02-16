6aab301ca7030ae8b06bb5328b7e250553afb9c3
Jesper Hodge <jesper@instaffo.de>
2019-02-16 22:52:33 +0100

    finish

--- mock_search.py
+++ mock_search.py
@@ -1,7 +1,62 @@
+from requests_html import HTMLSession
 import database
+import pdb
+
+class MorgenProvider:
+    def __init__(self, url):
+        self.session = HTMLSession()
+        try:
+            self.content = self.session.get(url)
+        except:
+            self.content = False
+
+
+    def result_titles(self):
+        if self.content:
+            try:
+                return self.content.html.find('.nfy-search-item h2')[:2]
+            except:
+                return False
+        else:
+            return False
+
+    def result_descriptions(self):
+        if self.content:
+            try:
+                return self.content.html.find('.nfy-search-item p')[:2]
+            except:
+                return False
+        else:
+            return False
+
+    def data(self):
+        data = [{}, {}]
+        if not self.result_titles() or not self.result_descriptions():
+            return False
+        for index, item in enumerate(self.result_titles()):
+            try:
+                data[index]['title'] = item.text
+            except:
+                next
+        for index, item in enumerate(self.result_descriptions()):
+            try:
+                data[index]['summary'] = item.text
+            except:
+                next
+        return data
+
+    def third_item(self):
+        try:
+            item_data = [{
+                'title': self.content.html.find('.nfy-search-item h2')[3].text,
+                'summary': self.content.html.find('.nfy-search-item p')[3].text
+                }]
+            return item_data
+        except:
+            return False
 
 class MockSearch:
-    mock_data = [{'title': 'Merkel rechnet in München mit Trump ab', 'summary': 'Frontalangriff der Kanzlerin auf US-Präsident Donald Trump: Angela Merkel hat auf der Münchner Sicherheitskonferenz die US-Politik scharf kritisiert.', 'tags': 'trump'}, {'title': 'Großer Widerstand gegen Trumps Notstandserklärung', 'summary': 'US-Präsident Donald Trump stößt mit seiner Erklärung eines Nationalen Notstandes an der Grenze zu Mexiko auf großen Widerstand. Mehrere prominente Demokraten warfen Trump einen Angriff auf die Verfassung vor.', 'tags': 'trump'},]
+    # Class Name is not accurate anymore: The search is real now.
 
     def __init__(self, search_term, user_id):
         self.term = search_term
@@ -10,8 +65,8 @@ class MockSearch:
         self.save_search()
 
     def mock_result(self):
-        result = self.mock_data
-        return result
+        result = MorgenProvider(self.search_url).data()
+        return result or False
 
     def save_search(self):
         connection = database.TestDB().connection
--- suggested_news.py
+++ suggested_news.py
@@ -1,22 +1,24 @@
 import database
+import mock_search
 
 class SuggestedNews:
-    mock_data = {'trump': [{'title': 'Vorwurf des Machtmissbrauchs', 'summary': 'WASHINGTON. Die Gefahr einer weiteren Haushaltssperre mit Verwaltungsstillstand in den USA ist vorerst abgewendet. Dafür haben sich die politischen Fronten massiv verhärtet.'}]}
-
     def __init__(self, user_id):
         self.user_id = user_id
         self.search_term = self.search_term()
 
     def search_url(self):
-        return f'https://www.morgenweb.de/suche_cosearch,{self.term}.html'
+        return f'https://www.morgenweb.de/suche_cosearch,{self.search_term}.html'
 
     def search_term(self):
         return self.find_latest_tag()
 
     def mock_result(self):
         if self.search_term:
-            result = self.mock_data.get(self.search_term, False)
-            return result
+            try:
+                res = mock_search.MorgenProvider(self.search_url()).third_item()
+                return res
+            except:
+                return False
         else:
             return False
 
@@ -25,8 +27,10 @@ class SuggestedNews:
         res_list = []
         for row in rows:
             if row[1] == self.user_id:
-                if self.mock_data.get(row[2], False):
+                try:
                     res_list.append(row[2])
+                except:
+                    return False
         if res_list:
             return(res_list[-1])
         else:
