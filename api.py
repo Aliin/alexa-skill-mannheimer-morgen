23746215f542911f3f0112bfd341dc07bb423ecd
kafaoglu <github@kafaoglu.de>
2019-02-16 14:11:37 +0100

    Events ready

--- api.py
+++ api.py
@@ -1,5 +1,22 @@
 import requests
+from flask_ask import Ask, statement, question, session
 
-def events_api():
+
+def events_api(params):
     host = "https://www2.morgenweb.de/austausch/vk_abfrage.php?out=json"
-    params = "test"
+    host_params = {"ab": params["datum"], "bis": params["datum"], "rubrik": params["rubrik"], "plz": params["plz"], "ort": params["ort"], "suche": params["suche"]}
+    for key, value in host_params.items():
+        if value is not None:
+            host += "&{}={}".format(key, value)
+
+    r = requests.get(host)
+
+    return r.json()["ergebnis"]
+
+def event_detail():
+    return None
+
+
+
+
+
--- app.py
+++ app.py
@@ -1,23 +1,186 @@
 from flask import Flask, render_template
-from flask_ask import Ask, statement, question
+from flask_ask import Ask, statement, question, session, request, context
+from api import events_api
+import random
+from datetime import datetime
 
 app = Flask(__name__)
 ask = Ask(app, '/')
 
+intro = "<audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_intro_01'/>"
+outro = "<audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_outro_01'/>"
+neutral_response = "<audio src='soundbank://soundlibrary/ui/gameshow/amzn_ui_sfx_gameshow_neutral_response_01'/>"
+background = "https://s3-eu-west-1.amazonaws.com/werbungmoma/mm_logo_upload.png"
+
 @ask.launch
 def launched():
     speech = render_template("welcome")
+    if context.System.device.supportedInterfaces.Display is None:
+        return question(speech)
+    else:
+        hint = "Was läuft heute in Mannheim im Kino?"
+        text = {"primaryText": {"type": "RichText", "text": ""}, "secondaryText": {"type": "RichText", "text": ""}}
+        return question(speech).display_render(template="BodyTemplate6", text=text, background_image_url=background, hintText=hint)
     return question(speech)
 
 @ask.intent("EventsIntent")
-def events():
-    speech = render_template("events")
-    return statement(speech)
+def events(datum, plz, ort, suche):
+    try:
+        rubrik = request["intent"]["slots"]["rubrik"]["resolutions"]["resolutionsPerAuthority"][0]["values"][0]["value"]["name"]
+    except:
+        rubrik = None
+    params = {"datum": datum, "rubrik": rubrik, "plz": plz, "ort": ort, "suche": suche}
+    try:
+        events_raw_data = events_api(params)
+        session_create(state="events", params=params, count=4)
+        events = iterate(events_raw_data)
+        speech = render_template("events", events=events["string"])
+        if context.System.device.supportedInterfaces.Display is None:
+            return question(speech)
+        else:
+            title = "Veranstaltungen"
+            items = get_items(events)
 
-@ask.intent("DummyIntent", convert={})
-def dummy_intent(name):
-    speech = "Freut mich {}".format(name)
-    return statement(speech)
+            return question(speech).list_display_render(template="ListTemplate1", backButton="HIDDEN", title=title,
+                                                        listItems=items)
+    except:
+        speech = "Es wurden keine Veranstaltungen gefunden."
+        if context.System.device.supportedInterfaces.Display is None:
+            return statement(speech)
+        else:
+            text = {"primaryText": {"type": "RichText", "text": "Es wurde keine Veranstaltung gefunden"}}
+            return statement(speech).display_render(template="BodyTemplate6", text=text, background_image_url=background)
+
+
+@ask.intent("AMAZON.YesIntent")
+def yes():
+    try:
+        if session.attributes["state"] == "end_of_data":
+            speech = "Das waren alle Einträge"
+            if context.System.device.supportedInterfaces.Display is None:
+                return statement(speech)
+            else:
+                text = {"primaryText": {"type": "RichText", "text": speech}}
+                return statement(speech).display_render(template="BodyTemplate6", text=text,
+                                                        background_image_url=background)
+        if session.attributes["state"] == "events":
+            event_raw_data = events_api(session.attributes["params"])
+            for index in range(0, session.attributes['count']):
+                try:
+                    event_raw_data.pop(0)
+                except:
+                    session.attributes["state"] = "end_of_data"
+
+
+            events = iterate(event_raw_data)
+
+            speech = render_template("events", events=events["string"])
+
+            if session.attributes["state"] == "end_of_data":
+                if context.System.device.supportedInterfaces.Display is None:
+                    return statement(speech)
+                else:
+                    title = "Veranstaltungen"
+                    items = get_items(events)
+
+                    return statement(speech).list_display_render(template="ListTemplate1", backButton="HIDDEN",
+                                                                title=title,
+                                                                listItems=items)
+            else:
+                if context.System.device.supportedInterfaces.Display is None:
+                    return question(speech)
+                else:
+                    title = "Veranstaltungen"
+                    items = get_items(events)
+
+                    return question(speech).list_display_render(template="ListTemplate1", backButton="HIDDEN",
+                                                                title=title,
+                                                                listItems=items)
+    except:
+        speech = "Est ist ein Fehler unterlaufen"
+        if context.System.device.supportedInterfaces.Display is None:
+            return statement(speech)
+        else:
+            text = {"primaryText": {"type": "RichText", "text": speech}}
+            return statement(speech).display_render(template="BodyTemplate6", text=text, background_image_url=background)
+
+@ask.intent("AMAZON.NoIntent")
+def no():
+    session_delete()
+    speech = "Tschüss"
+    if context.System.device.supportedInterfaces.Display is None:
+        return statement(speech)
+    else:
+        text = {"primaryText": {"type": "RichText", "text": speech}}
+        return statement(speech).display_render(template="BodyTemplate6", text=text, background_image_url=background)
+
+@app.template_filter()
+def iterate(events):
+    event_werbung = get_werbung()
+    four_events = {}
+    event_str = ""
+    for index, event in enumerate(events):
+        four_events[index] = event
+
+        if index == 0:
+            event_str += "{} <break time='1s'/> Beim Veranstalter {} in {} läuft {}".format(intro, event["veranstalter"], event["stadt"],
+                                                                     event["name_titel"])
+        elif index < 2:
+            event_str += "{} <break time='1s'/> Beim Veranstalter {} in {} läuft {}".format(neutral_response, event["veranstalter"],
+                                                                     event["stadt"], event["name_titel"])
+        else:
+            session.attributes["count"] += 3
+            if session.attributes['state'] == "end_of_data":
+                event_str += "{} <break time='1s'/> Beim Veranstalter {} in {} läuft {} <break time='0.5s'/> Werbung {} {}".format(
+                    neutral_response, event["veranstalter"], event["stadt"],
+                    event["name_titel"], event_werbung, outro)
+                break
+
+            else:
+                event_str += "{} <break time='1s'/> Beim Veranstalter {} in {} läuft {} <break time='0.5s'/> Werbung {} {} Möchtest Du noch mehr hören?".format(neutral_response, event["veranstalter"], event["stadt"],
+                                                                         event["name_titel"], event_werbung, outro)
+                break
+
+    return {"string": event_str, "liste": four_events}
+
+def get_werbung():
+    werbungen = ["https://s3-eu-west-1.amazonaws.com/werbungmoma/werbung1_alexa.mp3", "https://s3-eu-west-1.amazonaws.com/werbungmoma/werbung2_alexa.mp3", "https://s3-eu-west-1.amazonaws.com/werbungmoma/werbung3_alexa.mp3"]
+    ad = werbungen[random.randint(0, len(werbungen)-1)]
+    werbung = "<audio src='{}' />".format(ad)
+    return werbung
+
+def session_create(state, params, count):
+    session.attributes['state'] = state
+    session.attributes['params'] = params
+    session.attributes['count'] = count
+
+def session_delete():
+    session.attributes['state'] = ""
+    session.attributes['params'] = ""
+    session.attributes['count'] = ""
+
+def get_items(events):
+    items = []
+    for key, event in events["liste"].items():
+        beginnt = datetime.strptime(event["beginnt"], '%Y-%m-%d %H:%M:%S')
+        if beginnt.minute == 0:
+            minute = "00"
+        else:
+            minute = beginnt.minute
+
+        textcontent = {"primaryText": {"type": "RichText", "text": event["name_titel"]},
+                       "secondaryText": {"type": "RichText",
+                                         "text": "{} in {} {}, {} {}".format(event["veranstalter"], event["strasse"],
+                                                                             event["hausnummer"], event["plz"],
+                                                                             event["stadt"])},
+                       "tertiaryText": {"type": "RichText", "text": "{}:{}".format(beginnt.hour, minute)}}
+        item = {"textContent": textcontent}
+        items.append(item)
+
+    return items
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
