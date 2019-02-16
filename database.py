8ca15b55e9af359faea08582af5517f7029cc66f
Jesper Hodge <jesper@instaffo.de>
2019-02-16 15:57:59 +0100

    add database

--- database.py
+++ database.py
@@ -1,5 +1,19 @@
 import sqlite3
 
-conn = sqlite3.connect('test.db')
+class TestDB:
+    def __init__(self):
+        self.database = 'mm_test.db'
+        self.connection = connect(self)
 
-print("Opened database successfully")
+    def connect(self):
+        print("Opened database successfully")
+        return sqlite3.connect(self.database)
+
+    def setup(self):
+        self.connection.execute('''CREATE TABLE IF NOT EXISTS user_tags
+         (ID INT PRIMARY KEY        NOT NULL,
+         USER_UUID      CHAR(255)   NOT NULL,
+         TAG            CHAR(255)   NOT NULL);''')
+        print("Table created successfully")
+        self.connection.close()
+
